13e7b7f90745681ef5dab93af0621ee342a50476
Shuvam Mondal <96789016+Shuvam-M-Astro@users.noreply.github.com>
2023-11-08 08:54:32 +0100

    Create classification.py

---
+++ IRIS/classification.py
@@ -0,0 +1,27 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+
+# Load the IRIS dataset
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the classifier
+clf = LogisticRegression(random_state=42, max_iter=200)
+
+# Train the classifier
+clf.fit(X_train, y_train)
+
+# Make predictions
+y_pred = clf.predict(X_test)
+
+# Evaluate the classifier
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print(report)

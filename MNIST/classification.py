5d71cd9afcd130dc0978703bcbecedceccb29bf0
Shuvam Mondal <96789016+Shuvam-M-Astro@users.noreply.github.com>
2023-11-08 11:35:25 +0100

    Update classification.py

--- MNIST/classification.py
+++ MNIST/classification.py
@@ -1,27 +1,95 @@
-import pandas as pd
-from sklearn import datasets, svm, metrics
-from sklearn.model_selection import train_test_split
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+import time
 
-# Load the MNIST dataset
-digits = datasets.load_digits()
+# Check if GPU is available and set the device accordingly
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f'Using device: {device}')
 
-# Flatten the images, to turn data in a (samples, feature) matrix
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+# Define a transform to normalize the data
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5,), (0.5,))])  # MNIST is grayscale so we only need one channel
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+# Download and load the training data
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False)
+# Download and load the test data
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+# Define a simple CNN architecture suitable for MNIST
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)  # MNIST has only 1 channel
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+    def forward(self, x):
+        x = torch.relu(torch.max_pool2d(self.conv1(x), 2))
+        x = torch.relu(torch.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = torch.relu(self.fc1(x))
+        x = torch.dropout(x, p=0.5, train=self.training)
+        x = self.fc2(x)
+        return torch.log_softmax(x, dim=1)
 
-# Print the classification report
-report = metrics.classification_report(y_test, predicted)
-print(report)
+net = Net()
+net.to(device)
+
+# Define a Loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+
+# Train the network
+start_time = time.time()
+for epoch in range(2):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 100))
+            running_loss = 0.0
+
+end_time = time.time()
+print('Finished Training')
+print(f"Training time: {(end_time - start_time):.2f} seconds")
+
+# Save the trained model
+PATH = './mnist_net.pth'
+torch.save(net.state_dict(), PATH)
+
+# Test the network on the test data
+net = Net()
+net.load_state_dict(torch.load(PATH))
+net.to(device)
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        images, labels = images.to(device), labels.to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))

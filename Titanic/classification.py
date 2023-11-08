3d0aa0a8f046c1a12e6f096004ed6a7f2b6c4973
Shuvam Mondal <96789016+Shuvam-M-Astro@users.noreply.github.com>
2023-11-08 08:53:04 +0100

    Create classification.py

---
+++ Titanic/classification.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.impute import SimpleImputer
+
+# Load the dataset (replace 'titanic.csv' with the path to your Titanic dataset file)
+df = pd.read_csv('titanic.csv')
+
+# Preprocess the data
+# Fill missing values
+imputer = SimpleImputer(strategy='mean')
+df['Age'] = imputer.fit_transform(df[['Age']])
+
+# Convert categorical variables into dummy/indicator variables
+df = pd.get_dummies(df, columns=['Sex', 'Embarked', 'Pclass'])
+
+# Select features and target
+features = ['Age', 'SibSp', 'Parch', 'Fare', 'Sex_female', 'Sex_male', 'Embarked_C', 'Embarked_Q', 'Embarked_S', 'Pclass_1', 'Pclass_2', 'Pclass_3']
+target = 'Survived'
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size=0.2, random_state=42)
+
+# Initialize the classifier
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the classifier
+clf.fit(X_train, y_train)
+
+# Make predictions
+y_pred = clf.predict(X_test)
+
+# Evaluate the classifier
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print(report)

e2baf08e1404afb1356c167ecd8c5eb438c9d9d2
Shuvam Mondal <96789016+Shuvam-M-Astro@users.noreply.github.com>
2023-11-24 14:48:04 +0100

    Update class.py

--- AIR/class.py
+++ AIR/class.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from torch.utils.data import DataLoader, TensorDataset
 

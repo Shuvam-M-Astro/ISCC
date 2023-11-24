d92efa1158e0836b63fe1a5d117b3f5d13cb34fe
Shuvam Mondal <96789016+Shuvam-M-Astro@users.noreply.github.com>
2023-11-24 14:48:52 +0100

    Update class.py

--- AIR/class.py
+++ AIR/class.py
@@ -1,10 +1,7 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from torch.utils.data import DataLoader, TensorDataset
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 # Load dataset
 url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/airline-passengers.csv'
@@ -12,58 +9,16 @@ data = pd.read_csv(url)
 data['Month'] = pd.to_datetime(data['Month'])
 data.set_index('Month', inplace=True)
 
-# Scaling data
-scaler = MinMaxScaler(feature_range=(-1, 1))
-data_scaled = scaler.fit_transform(data.values)
+# Prepare data for linear regression
+data['Time'] = np.arange(len(data.index))
+X = data['Time'].values.reshape(-1, 1)
+y = data['Passengers'].values
 
-# Prepare data for PyTorch model
-def create_inout_sequences(input_data, tw):
-    inout_seq = []
-    L = len(input_data)
-    for i in range(L-tw):
-        train_seq = input_data[i:i+tw]
-        train_label = input_data[i+tw:i+tw+1]
-        inout_seq.append((train_seq ,train_label))
-    return inout_seq
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-seq_length = 5
-train_inout_seq = create_inout_sequences(data_scaled, seq_length)
+# Create and train the model
+model = LinearRegression()
+model.fit(X_train, y_train)
 
-# Define the model
-class LSTM(nn.Module):
-    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
-        super().__init__()
-        self.hidden_layer_size = hidden_layer_size
-        self.lstm = nn.LSTM(input_size, hidden_layer_size)
-        self.linear = nn.Linear(hidden_layer_size, output_size)
-        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
-                            torch.zeros(1,1,self.hidden_layer_size))
-
-    def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
-        predictions = self.linear(lstm_out.view(len(input_seq), -1))
-        return predictions[-1]
-
-# Instantiate the model, define the loss function and the optimizer
-model = LSTM()
-loss_function = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-# Train the model
-epochs = 150
-for i in range(epochs):
-    for seq, labels in train_inout_seq:
-        optimizer.zero_grad()
-        model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
-                        torch.zeros(1, 1, model.hidden_layer_size))
-
-        y_pred = model(seq)
-
-        single_loss = loss_function(y_pred, labels)
-        single_loss.backward()
-        optimizer.step()
-
-    if i%25 == 1:
-        print(f'epoch: {i:3} loss: {single_loss.item():10.8f}')
-
-print(f'epoch: {i:3} loss: {single_loss.item():10.10f}')
+# Model is now trained, you can make predictions using model.predict()

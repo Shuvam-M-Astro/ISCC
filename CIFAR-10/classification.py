d71d61f0bb572c79717d8631b6694f6fbb1c22be
Shuvam Mondal <96789016+Shuvam-M-Astro@users.noreply.github.com>
2023-11-25 14:21:46 +0100

    Update classification.py

--- CIFAR-10/classification.py
+++ CIFAR-10/classification.py
@@ -1,24 +1,108 @@
-from numba import cuda
-import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+import time
 
-@cuda.jit
-def add_kernel(x, y, out):
-    tx = cuda.threadIdx.x
-    ty = cuda.blockIdx.x
-    bw = cuda.blockDim.x
-    pos = tx + ty * bw
-    if pos < x.size:
-        out[pos] = x[pos] + y[pos]
 
-def add_with_cuda(x, y):
-    out = np.empty_like(x)
-    threads_per_block = 32
-    blocks_per_grid = (x.size + (threads_per_block - 1)) // threads_per_block
-    add_kernel[blocks_per_grid, threads_per_block](x, y, out)
-    return out
+print(torch.cuda.is_available())  # Should return True if CUDA is set up correctly
+print(torch.version.cuda)         # Should display '11.5'
 
-# Example usage
-x = np.random.rand(128).astype(np.float32)
-y = np.random.rand(128).astype(np.float32)
-result = add_with_cuda(x, y)
-print(result)
+print(torch.__version__)
+
+# Set the device to CPU
+device = torch.device("cpu")
+print(f'Using device: {device}')
+
+# Define a transform to normalize the data
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# Download and load the training data
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=0)
+
+# Download and load the test data
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=0)
+
+# Define a simple CNN architecture
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except the batch dimension
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+net.to(device)
+
+# Define a Loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+# Train the network
+start_time = time.time()
+for epoch in range(2):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+end_time = time.time()
+print('Finished Training')
+print(f"Training time: {(end_time - start_time):.2f} seconds")
+
+# Save the trained model
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+# Test the network on the test data
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+net = Net()
+net.load_state_dict(torch.load(PATH))
+net.to(device)
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
